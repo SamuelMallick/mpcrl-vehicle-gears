03f6d8248bb585e07575ebf15a950d5fac25fb05
SamuelMallick <sam.mallick.97@gmail.com>
2025-01-30 17:33:35 +0100

    random traj to env

--- env.py
+++ env.py
@@ -5,10 +5,10 @@ from vehicle import Vehicle
 
 class VehicleTracking(gym.Env):
 
-    ts = 0.1  # sample time (s)
+    ts = 1  # sample time (s)
     alpha = 0  # road inclination (rad)
 
-    gamma = 0.01  # weight for tracking in cost
+    gamma = 0.1  # weight for tracking in cost
     Q = np.array([[1, 0], [0, 0.1]])  # tracking cost weight
 
     def __init__(self, vehicle: Vehicle):
@@ -27,20 +27,25 @@ class VehicleTracking(gym.Env):
             self.vehicle.x = np.array([[0], [10]])
             self.x = self.vehicle.x
 
-        d_ref = (self.x[0] + 20 + 20 * self.ts * np.arange(0, 500)).reshape(
-            500, 1, 1
-        )  # TODO get rid of hard code 500
-        v_ref = np.full((500, 1, 1), 20)
-        self.x_ref = np.concatenate((d_ref, v_ref), axis=1)
+        # d_ref = (self.x[0] + 20 + 20 * self.ts * np.arange(0, 500)).reshape(
+        #     500, 1, 1
+        # )  # TODO get rid of hard code 500
+        # v_ref = np.full((500, 1, 1), 20)
+        # self.x_ref = np.concatenate((d_ref, v_ref), axis=1)
+        self.x_ref = self.generate_random_x_ref()
         self.counter = 0
 
         return self.x, {}
 
     def reward(self, x: np.ndarray, fuel: float) -> float:
         # TODO add docstring
-        return self.gamma * (x - self.x_ref[self.counter]).T @ self.Q @ (
-            x - self.x_ref[self.counter]
-        ) + fuel
+        return (
+            self.gamma
+            * (x - self.x_ref[self.counter]).T
+            @ self.Q
+            @ (x - self.x_ref[self.counter])
+            + fuel
+        )
 
     def step(
         self, action: tuple[float, float, int]
@@ -67,3 +72,28 @@ class VehicleTracking(gym.Env):
     def get_x_ref_prediction(self, horizon: int) -> np.ndarray:
         # TODO add docstring
         return self.x_ref[self.counter : self.counter + horizon]
+
+    def generate_random_x_ref(self):
+        # TODO add docstring
+        len = 110  # TODO get rid of hard code 500
+        x_ref = np.zeros((len, 2, 1))
+        v = 20 + self.np_random.uniform(-5, 5)
+        x_ref[0] = np.array([[self.x[0, 0]], [v]])
+        change_points = np.sort(self.np_random.integers(-5, 5, size=4))
+        slopes = self.np_random.uniform(-0.6, 0.6, size=3)
+
+        for k in range(20 + change_points[0]):
+            x_ref[k + 1] = np.array([[x_ref[k, 0, 0] + self.ts * v], [v]])
+        for k in range(20 + change_points[0], 35 + change_points[1]):
+            v = max(min(35, v + slopes[0]), 5)
+            x_ref[k + 1] = np.array([[x_ref[k, 0, 0] + self.ts * v], [v]])
+        for k in range(35 + change_points[1], 50 + change_points[2]):
+            v = max(min(35, v + slopes[1]), 5)
+            x_ref[k + 1] = np.array([[x_ref[k, 0, 0] + self.ts * v], [v]])
+        for k in range(50 + change_points[2], 70 + change_points[3]):
+            v = max(min(35, v + slopes[2]), 5)
+            x_ref[k + 1] = np.array([[x_ref[k, 0, 0] + self.ts * v], [v]])
+        for k in range(70 + change_points[3], len - 1):
+            x_ref[k + 1] = np.array([[x_ref[k, 0, 0] + self.ts * v], [v]])
+
+        return x_ref
