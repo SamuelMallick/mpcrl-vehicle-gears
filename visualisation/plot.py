f5cebadfaac88c2dceb694ba3a7bf319cf1c8092
SamuelMallick <sam.mallick.97@gmail.com>
2025-02-06 14:33:54 +0100

    update plot file

--- visualisation/plot.py
+++ visualisation/plot.py
@@ -16,13 +16,11 @@ def plot_training(
         np.convolve(cost, np.ones(average_interval) / average_interval, mode="valid")
     )
     ax[0].set_ylabel("Cost")
-    ax[0].set_yscale("log")
     # ax[1].plot(fuel)
     ax[1].plot(
         np.convolve(fuel, np.ones(average_interval) / average_interval, mode="valid")
     )
     ax[1].set_ylabel("Fuel")
-    ax[1].set_yscale("log")
     # ax[2].plot(tracking)
     ax[2].plot(
         np.convolve(
@@ -30,7 +28,6 @@ def plot_training(
         )
     )
     ax[2].set_ylabel("Tracking")
-    ax[2].set_yscale("log")
     # ax[3].plot(penalty)
     ax[3].plot(
         np.convolve(penalty, np.ones(average_interval) / average_interval, mode="valid")
@@ -50,20 +47,21 @@ def plot_evaluation(
     w_e: np.ndarray,
 ):
     # TODO add docstring
-    fig, ax = plt.subplots(4, 1, sharex=True)
+    fig, ax = plt.subplots(5, 1, sharex=True)
     ax[0].plot(x_ref[:, 0] - X[:-1, 0])
-    # ax[0].plot(x_ref[:, 0])
-    # ax[0].legend(["actual", "desired"])
     ax[0].set_ylabel("d_e (m)")
-    ax[1].plot(X[:, 1])
-    ax[1].plot(x_ref[:, 1])
+    ax[1].plot(X[:, 0])
+    ax[1].plot(x_ref[:, 0])
     ax[1].legend(["actual", "desired"])
-    ax[1].set_ylabel("v (m/s)")
-    ax[2].plot(np.cumsum(fuel))
-    ax[2].set_ylabel("Fuel (L)")
+    ax[2].plot(X[:, 1])
+    ax[2].plot(x_ref[:, 1])
+    ax[2].legend(["actual", "desired"])
+    ax[2].set_ylabel("v (m/s)")
+    ax[3].plot(np.cumsum(fuel))
+    ax[3].set_ylabel("Fuel (L)")
     # ax[3].plot(np.cumsum(R))
-    ax[3].plot(R)
-    ax[3].set_ylabel("Reward")
+    ax[4].plot(R)
+    ax[4].set_ylabel("Reward")
 
     fig, ax = plt.subplots(4, 1, sharex=True)
     ax[0].plot(T_e)  # T_e actual
