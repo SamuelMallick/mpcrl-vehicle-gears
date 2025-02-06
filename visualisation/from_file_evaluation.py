b3e9dfa8a3e98d20f7428157ed0fdcc232b46726
SamuelMallick <sam.mallick.97@gmail.com>
2025-02-06 17:12:30 +0100

    update eval plot

--- visualisation/from_file_evaluation.py
+++ visualisation/from_file_evaluation.py
@@ -4,7 +4,7 @@ import sys, os
 sys.path.append(os.getcwd())
 from visualisation.plot import plot_evaluation, plot_training
 
-file_name = "results/evaluations/MIQP_MPC.pkl"
+file_name = "results/evaluations/MIQP_MPC_N_20.pkl"
 with open(file_name, "rb") as f:
     data = pickle.load(f)
 
@@ -16,9 +16,14 @@ U = data["U"]
 x_ref = data["x_ref"]
 engine_torque = data["T_e"]
 engine_speed = data["w_e"]
+mpc_solve_time = data["mpc_solve_time"]
 num_eps = len(R)
 
-for ep in range(num_eps):
-    plot_evaluation(
-        x_ref[ep], X[ep], U[ep], R[ep], fuel[ep], engine_torque[ep], engine_speed[ep]
-    )
+print(f"average cost = {sum([sum(R[i]) for i in range(len(R))]) / len(R)}")
+print(f"average fuel = {sum([sum(fuel[i]) for i in range(len(fuel))]) / len(fuel)}")
+print(f"total mpc solve times = {sum(mpc_solve_time)}")
+
+# for ep in range(num_eps):
+#     plot_evaluation(
+#         x_ref[ep], X[ep], U[ep], R[ep], fuel[ep], engine_torque[ep], engine_speed[ep]
+#     )
