BIG BUG fix that scaling was not applied to mpc's. Also limit velocity to 20-100 kmh for references

env.py
@@ -25,7 +25,7 @@ class VehicleTracking(gym.Env):
     ts = 1  # sample time (s)
     alpha = 0  # road inclination (rad)
 
-    gamma = 0.1  # weight for tracking in cost
+    gamma = 0.01  # weight for tracking in cost
     Q = np.array([[1, 0], [0, 0.1]])  # tracking cost weight
 
     def __init__(
@@ -116,7 +116,7 @@ class VehicleTracking(gym.Env):
                 self.np_random.integers(0, len - 1, size=n_segments - 1)
             )
             change_points = np.concatenate(([0], intermediate_change_points, [len - 1]))
-            v_clip_range = [5, 35]
+            v_clip_range = [5, 28]  # 18 - 100 km/h
 
             # generate initial conditions and acceleration profiles
             if trajectory_type == "type_2":

mpc.py
@@ -65,7 +65,7 @@ np.fill_diagonal(A[:, 1:], 1)
 
 # cost matrix for tracking
 Q = cs.diag([1, 0.1])
-gamma = 0.1  # weight for tracking in cost
+gamma = 0.01  # weight for tracking in cost
 
 
 def nonlinear_hybrid_model(x: cs.SX, u: cs.SX, dt: float, alpha: float) -> cs.SX:
@@ -316,13 +316,15 @@ class HybridTrackingMpc(Mpc):
 
         x_ref = self.parameter("x_ref", (2, prediction_horizon + 1))
         self.minimize(
-            sum(
+            gamma
+            * sum(
                 [
                     cs.mtimes([(x[:, i] - x_ref[:, i]).T, Q, x[:, i] - x_ref[:, i]])
                     for i in range(prediction_horizon + 1)
                 ]
             )
             + fuel_cost
+            # + sum(0.01 * F_b[i] for i in range(prediction_horizon))
         )
         if (
             convexify_dynamics
@@ -447,7 +449,8 @@ class HybridTrackingFuelMpcFixedGear(Mpc):
             fuel_cost = 0
 
         self.minimize(
-            sum(
+            gamma
+            * sum(
                 [
                     cs.mtimes([(x[:, i] - x_ref[:, i]).T, Q, x[:, i] - x_ref[:, i]])
                     for i in range(prediction_horizon + 1)
@@ -505,7 +508,8 @@ class TrackingMpc(Mpc):
         x_ref = self.parameter("x_ref", (2, prediction_horizon + 1))
         self.set_nonlinear_dynamics(lambda x, u: nonlinear_model(x, u, dt, 0))
         self.minimize(
-            sum(
+            gamma
+            * sum(
                 [
                     cs.mtimes([(x[:, i] - x_ref[:, i]).T, Q, x[:, i] - x_ref[:, i]])
                     for i in range(prediction_horizon + 1)