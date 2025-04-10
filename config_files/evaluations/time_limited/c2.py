a46ff45ed49cad32953e0a10166f074f521a6ba2
S. H. Mallick <45310725+SamuelMallick@users.noreply.github.com>
2025-04-10 15:06:10 +0200

    new configs

---
+++ config_files/evaluations/time_limited/c2.py
@@ -0,0 +1,14 @@
+from config_files.base import ConfigDefault
+
+
+class Config(ConfigDefault):
+
+    def __init__(self):
+        super().__init__()
+        self.id = "eval_2"
+        self.eval_seed = 11
+        max_time = 1
+        self.extra_opts["gurobi"]["TimeLimit"] = max_time
+        self.extra_opts["knitro"]["maxtime"] = max_time
+        self.extra_opts["bonmin"]["time_limit"] = max_time
+        self.extra_opts["ipopt"]["max_wall_time"] = max_time
