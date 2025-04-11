004ed8a63a2dba415aa23913d87891fc8536626c
S. H. Mallick <45310725+SamuelMallick@users.noreply.github.com>
2025-04-11 11:40:28 +0200

    configs

--- config_files/multi_starts/knitro/c5.py
+++ config_files/multi_starts/knitro/c5.py
@@ -7,7 +7,7 @@ class Config(ConfigDefault):
         super().__init__()
         self.id = "multi_starts_knitro_5"
         self.multi_starts = 1
-        max_time = 10
+        max_time = 50
         self.extra_opts["gurobi"]["TimeLimit"] = max_time
         self.extra_opts["knitro"]["maxtime"] = max_time
         self.extra_opts["bonmin"]["time_limit"] = max_time
