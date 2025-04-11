9697f243ece19b2756afdaaa0b34a273a557a23e
S. H. Mallick <45310725+SamuelMallick@users.noreply.github.com>
2025-04-11 09:52:32 +0200

    config files

--- config_files/multi_starts/knitro/c2.py
+++ config_files/multi_starts/knitro/c2.py
@@ -7,7 +7,7 @@ class Config(ConfigDefault):
         super().__init__()
         self.id = "multi_starts_knitro_2"
         self.multi_starts = 5
-        max_time = 5
+        max_time = 10
         self.extra_opts["gurobi"]["TimeLimit"] = max_time
         self.extra_opts["knitro"]["maxtime"] = max_time
         self.extra_opts["bonmin"]["time_limit"] = max_time
--- config_files/multi_starts/knitro/c7.py
+++ config_files/multi_starts/knitro/c7.py
@@ -5,10 +5,12 @@ class Config(ConfigDefault):
 
     def __init__(self):
         super().__init__()
-        self.id = "multi_starts_knitro_1"
+        self.id = "multi_starts_knitro_7"
         self.multi_starts = 1
         max_time = 10
         self.extra_opts["gurobi"]["TimeLimit"] = max_time
         self.extra_opts["knitro"]["maxtime"] = max_time
         self.extra_opts["bonmin"]["time_limit"] = max_time
         self.extra_opts["ipopt"]["max_wall_time"] = max_time
+
+        self.extra_opts["knitro"]["ms_maxsolves"] = 50
