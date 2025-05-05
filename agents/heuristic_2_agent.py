1499506d411f2742f6bec01d73b7871125a4846d
S. H. Mallick <sam.mallick.97@gmail.com>
2025-05-05 15:07:10 +0200

    parallel gear schedues

--- agents/heuristic_2_agent.py
+++ agents/heuristic_2_agent.py
@@ -34,19 +34,15 @@ class Heuristic2Agent(SingleVehicleAgent):
         super().__init__(mpc, np_random=np_random, multi_starts=multi_starts)
 
     def solve_mpc(self, pars, vals0) -> tuple[float, float, int, dict]:
-        sol = self.mpc.solve(pars, vals0)
+        sol, info = self.mpc.solve(pars, vals0)
         if not sol.success:
             raise ValueError("MPC failed to solve")
         T_e = sol.vals["T_e"].full()[0, 0]
         F_b = sol.vals["F_b"].full()[0, 0]
-        gear = np.argmax(pars["gear"], 0)[0]
+        gear = np.argmax(pars[info["best_indx"]]["gear"], 0)[0]
         return T_e, F_b, gear, {"sol": sol}
 
     def get_action(self, state: np.ndarray) -> tuple[float, float, int, dict]:
-        best_sol = None
-        best_T_e = None
-        best_F_b = None
-        best_gear = None
         gears = []
         vals0 = (
             [self.prev_sol.vals]
@@ -54,27 +50,25 @@ class Heuristic2Agent(SingleVehicleAgent):
             if self.prev_sol
             else self.initial_guesses_vals(state, self.multi_starts)
         )
+        pars = []
         for gear_priority in self.gear_priority:
             gear = self.gear_from_velocity(state[1].item(), gear_priority)
             if gear not in gears:
                 gears.append(gear)
                 gear_choice_binary = np.zeros((6, self.mpc.prediction_horizon))
                 gear_choice_binary[gear] = 1
-                pars = {
-                    "x_0": state,
-                    "x_ref": self.x_ref_predicition.T.reshape(2, -1),
-                    "T_e_prev": self.T_e_prev,
-                    "gear": gear_choice_binary,
-                    "gear_prev": self.gear_prev,
-                }
-                T_e, F_b, gear, info = self.solve_mpc(pars, vals0)
-                if best_sol is None or info["sol"].f < best_sol.f:
-                    best_sol = info["sol"]
-                    best_T_e = T_e
-                    best_F_b = F_b
-                    best_gear = gear
-        self.prev_sol = self.shift_sol(best_sol)
-        return best_T_e, best_F_b, best_gear, {}
+                pars.append(
+                    {
+                        "x_0": state,
+                        "x_ref": self.x_ref_predicition.T.reshape(2, -1),
+                        "T_e_prev": self.T_e_prev,
+                        "gear": gear_choice_binary,
+                        "gear_prev": self.gear_prev,
+                    }
+                )
+        T_e, F_b, gear, info = self.solve_mpc(pars, vals0)
+        self.prev_sol = self.shift_sol(info["sol"])
+        return T_e, F_b, gear, {}
 
 
 class DistributedHeuristic2Agent(PlatoonAgent, Heuristic2Agent):
@@ -110,28 +104,21 @@ class DistributedHeuristic2Agent(PlatoonAgent, Heuristic2Agent):
                 if self.prev_sols[i]
                 else self.initial_guesses_vals(x, self.multi_starts)
             )
-            best_sol = None
-            best_T_e = None
-            best_F_b = None
-            best_gear = None
             gears = []
+            _pars = []
             for gear_priority in self.gear_priority:
                 gear = self.gear_from_velocity(x[1].item(), gear_priority)
                 if gear not in gears:
                     gears.append(gear)
                     gear_choice_binary = np.zeros((6, self.mpc.prediction_horizon))
                     gear_choice_binary[gear] = 1
-                    _pars = {**pars, "gear": gear_choice_binary}
-                    T_e, F_b, gear, info = self.solve_mpc(_pars, vals0)
-                    if best_sol is None or info["sol"].f < best_sol.f:
-                        best_sol = info["sol"]
-                        best_T_e = T_e
-                        best_F_b = F_b
-                        best_gear = gear
-            T_e_list.append(best_T_e)
-            F_b_list.append(best_F_b)
-            gear_list.append(best_gear)
-            self.prev_sols[i] = best_sol
+                    _pars.append({**pars, "gear": gear_choice_binary})
+            T_e, F_b, gear, info = self.solve_mpc(_pars, vals0)
+
+            T_e_list.append(T_e)
+            F_b_list.append(F_b)
+            gear_list.append(gear)
+            self.prev_sols[i] = info["sol"]
         for i in range(self.num_vehicles):
             self.prev_sols[i] = self.shift_sol(self.prev_sols[i])
         return np.asarray(T_e_list), np.asarray(F_b_list), gear_list, {}
