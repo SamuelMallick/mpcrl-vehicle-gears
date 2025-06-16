d7533007dbe7820e9e111c1d6392fd24c6629b2f
gbattocletti <g.battocletti@tudelft.nl>
2025-06-16 15:37:45 +0200

    Add option to plot individual training curves

--- visualisation/plot_training.py
+++ visualisation/plot_training.py
@@ -1,22 +1,25 @@
+import os
 import pickle
-import sys, os
-from matplotlib import pyplot as plt
-import pandas as pd
+import sys
+
 import numpy as np
+import pandas as pd
 import seaborn as sns
+from matplotlib import pyplot as plt
 
 sys.path.append(os.getcwd())
 # from utils.tikz import save2tikz
 
 skip = 10000
 average_interval = 10000
+show_individual_lines = False
 
 file_names = [
-    "dev/results/3/seeds/1/data_step_5000000.pkl",
-    "dev/results/3/seeds/2/data_step_5000000.pkl",
-    "dev/results/3/seeds/3/data_step_5000000.pkl",
-    "dev/results/3/seeds/4/data_step_5000000.pkl",
-    "dev/results/3/seeds/5/data_step_5000000.pkl",
+    "dev/results/1/seeds/1/data_step_5000000.pkl",
+    "dev/results/1/seeds/2/data_step_5000000.pkl",
+    "dev/results/1/seeds/3/data_step_5000000.pkl",
+    "dev/results/1/seeds/4/data_step_5000000.pkl",
+    "dev/results/1/seeds/5/data_step_5000000.pkl",
 ]
 
 L = []
@@ -44,24 +47,48 @@ for file_name in file_names:
     # kappa.append(heuristic)
 
 data = [L, L_t, L_f, kappa]
-data_avg = [np.array(
-    [
-        np.convolve(l, np.ones(average_interval) / average_interval, mode="valid")
-        for l in d
-    ]
-)[:, ::skip] for d in data]
+data_avg = [
+    np.array(
+        [
+            np.convolve(l, np.ones(average_interval) / average_interval, mode="valid")
+            for l in d
+        ]
+    )[:, ::skip]
+    for d in data
+]
 data_df = [pd.DataFrame(d.T, columns=file_names) for d in data_avg]
 for d in data_df:
     d["x"] = np.arange(len(d))
 data_df_long = [d.melt(id_vars="x", var_name="seed", value_name="L") for d in data_df]
 
-fig, ax = plt.subplots(4, 1, sharex=True)
-sns.lineplot(data=data_df_long[0], x="x", y="L", errorbar="sd", ax=ax[0])
-ax[0].set_ylabel("L")
-sns.lineplot(data=data_df_long[1], x="x", y="L", errorbar="sd", ax=ax[1])
-ax[1].set_ylabel("L_t")
-sns.lineplot(data=data_df_long[2], x="x", y="L", errorbar="sd", ax=ax[2])
-ax[2].set_ylabel("L_f")
-sns.lineplot(data=data_df_long[3], x="x", y="L", errorbar="sd", ax=ax[3])
-ax[3].set_ylabel("kappa")
-plt.show()
+# Plot results
+if show_individual_lines:
+    fig, ax = plt.subplots(4, 1, sharex=True)
+    sns.lineplot(
+        data=data_df_long[0], x="x", y="L", errorbar="sd", ax=ax[0], hue="seed"
+    )
+    ax[0].set_ylabel("L")
+    sns.lineplot(
+        data=data_df_long[1], x="x", y="L", errorbar="sd", ax=ax[1], hue="seed"
+    )
+    ax[1].set_ylabel("L_t")
+    sns.lineplot(
+        data=data_df_long[2], x="x", y="L", errorbar="sd", ax=ax[2], hue="seed"
+    )
+    ax[2].set_ylabel("L_f")
+    sns.lineplot(
+        data=data_df_long[3], x="x", y="L", errorbar="sd", ax=ax[3], hue="seed"
+    )
+    ax[3].set_ylabel("kappa")
+    plt.show()
+else:
+    fig, ax = plt.subplots(4, 1, sharex=True)
+    sns.lineplot(data=data_df_long[0], x="x", y="L", errorbar="sd", ax=ax[0])
+    ax[0].set_ylabel("L")
+    sns.lineplot(data=data_df_long[1], x="x", y="L", errorbar="sd", ax=ax[1])
+    ax[1].set_ylabel("L_t")
+    sns.lineplot(data=data_df_long[2], x="x", y="L", errorbar="sd", ax=ax[2])
+    ax[2].set_ylabel("L_f")
+    sns.lineplot(data=data_df_long[3], x="x", y="L", errorbar="sd", ax=ax[3])
+    ax[3].set_ylabel("kappa")
+    plt.show()
