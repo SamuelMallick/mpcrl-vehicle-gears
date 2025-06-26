4c3f19fc22cdb4ddc702ed07d5882cf35318e75f
gbattocletti <g.battocletti@tudelft.nl>
2025-06-26 14:46:26 +0200

    Fix problems and add verbosity

--- run_eval_single.sh
+++ run_eval_single.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check if an argument was provided
+# Check if the right number of arguments is provided
 if [ "$#" -lt 1 ]; then
   echo "Missing argument. Usage: . run_eval_single.sh <argument>"
   return
@@ -9,23 +9,28 @@ if [ "$#" -gt 1 ]; then
   echo "Too many arguments. Usage: . run_eval_single.sh <argument>"
   return
 fi
-if [ ! -f "run_single_vehicle/$ARGUMENT" ]; then
-    echo "Error: File 'run_single_vehicle/$ARGUMENT' does not exist."
-    return
-fi
 
-# Get the first argument
-ARGUMENT="$1"
+# Get the first argument and check that the file exists
+ARGUMENT=$1
+if ! [ -f "run_single_vehicle/$ARGUMENT" ]; then
+  echo "Error: File 'run_single_vehicle/$ARGUMENT' does not exist."
+  return
+fi
 
 # Run evaluations
 echo "Running evaluation of: run_single_vehicle/$ARGUMENT"
-echo "\r########### SEED 1 ###########"
+echo -e "\n########### SEED 1 ###########"
 python "run_single_vehicle/$ARGUMENT" eval_seeds.eval_seed1
-echo "\r########### SEED 2 ###########"
+echo -e "\n########### SEED 2 ###########"
 python "run_single_vehicle/$ARGUMENT" eval_seeds.eval_seed2
-echo "\r########### SEED 3 ###########"
+echo -e "\n########### SEED 3 ###########"
 python "run_single_vehicle/$ARGUMENT" eval_seeds.eval_seed3
-echo "\r########### SEED 4 ###########"
+echo -e "\n########### SEED 4 ###########"
 python "run_single_vehicle/$ARGUMENT" eval_seeds.eval_seed4
-echo "\r########### SEED 5 ###########"
+echo -e "\n########### SEED 5 ###########"
 python "run_single_vehicle/$ARGUMENT" eval_seeds.eval_seed5
+
+echo -e "\nEvaluation completed."
+
+# Clean up the argument variable
+unset ARGUMENT
