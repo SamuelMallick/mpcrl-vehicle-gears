e7d056f7292355fed9a97a5479cb1c28d24681b9
gbattocletti <g.battocletti@tudelft.nl>
2025-06-26 15:09:44 +0200

    Add iteration over seeds

--- run_eval_single.sh
+++ run_eval_single.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
 # Check if the right number of arguments is provided
-if [ "$#" -lt 1 ]; then
-  echo "Missing argument. Usage: . run_eval_single.sh <argument>"
+if [ "$#" -lt 2 ]; then
+  echo "Missing argument. Usage: . run_eval_single.sh <filename> <end_seed>"
   return
 fi
-if [ "$#" -gt 1 ]; then
-  echo "Too many arguments. Usage: . run_eval_single.sh <argument>"
+if [ "$#" -gt 2 ]; then
+  echo "Too many arguments. Usage: . run_eval_single.sh <filename> <end_seed>"
   return
 fi
 
 # Get the first argument and check that the file exists
-ARGUMENT=$1
-if ! [ -f "run_single_vehicle/$ARGUMENT" ]; then
-  echo "Error: File 'run_single_vehicle/$ARGUMENT' does not exist."
+FILENAME=$1
+if ! [ -f "run_single_vehicle/$FILENAME" ]; then
+  echo "Error: File 'run_single_vehicle/$FILENAME' does not exist."
+  return
+fi
+SEED_MAX=$2
+if ! [[ "$SEED_MAX" =~ ^[0-9]+$ ]]; then
+  echo "Error: Second argument must be an integer number (end seed)."
   return
 fi
 
 # Run evaluations
-echo "Running evaluation of: run_single_vehicle/$ARGUMENT"
-echo -e "\n########### SEED 1 ###########"
-python "run_single_vehicle/$ARGUMENT" eval_seeds.eval_seed1
-echo -e "\n########### SEED 2 ###########"
-python "run_single_vehicle/$ARGUMENT" eval_seeds.eval_seed2
-echo -e "\n########### SEED 3 ###########"
-python "run_single_vehicle/$ARGUMENT" eval_seeds.eval_seed3
-echo -e "\n########### SEED 4 ###########"
-python "run_single_vehicle/$ARGUMENT" eval_seeds.eval_seed4
-echo -e "\n########### SEED 5 ###########"
-python "run_single_vehicle/$ARGUMENT" eval_seeds.eval_seed5
+echo "Running evaluation of: run_single_vehicle/$FILENAME"
+for SEED in $( eval echo {1..$SEED_MAX} ); do
+  echo -e "\n########### SEED $SEED ###########"
+  taskset -c 0-7 python "run_single_vehicle/$FILENAME" eval_seeds.eval_seed$SEED
+done
 
 echo -e "\nEvaluation completed."
 
 # Clean up the argument variable
-unset ARGUMENT
+unset FILENAME
+unset SEED_MAX
+unset SEED
