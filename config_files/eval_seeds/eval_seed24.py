7875421915171db8386486bb765209067aa9c6a1
gbattocletti <g.battocletti@tudelft.nl>
2025-06-26 16:23:41 +0200

    Add new eval config files

---
+++ config_files/eval_seeds/eval_seed24.py
@@ -0,0 +1,9 @@
+from config_files.eval_seeds.eval_base import EvalBase
+
+
+class Config(EvalBase):
+
+    def __init__(self):
+        super().__init__()
+        self.id = "eval_seed24"
+        self.eval_seed = 124
