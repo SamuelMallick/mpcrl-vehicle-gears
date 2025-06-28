f6cd15cc6dde17d159dee8efea67795e0e85c505
gbattocletti <g.battocletti@tudelft.nl>
2025-06-28 12:30:54 +0200

    Comment out lines causing issues in knitro evaluation

--- config_files/eval_seeds/eval_base.py
+++ config_files/eval_seeds/eval_base.py
@@ -8,11 +8,19 @@ class EvalBase(ConfigDefault):
         self.id = "eval_base"
         self.eval_seed = 0
 
-        self.multi_starts = 1  # Should this be changed?
+        # Toggle for use of bidirectional RNN
+        # NOTE: This value must be kept constant during a set of experiments, i.e., a
+        # batch of experiments that runs at the same time must be either formed only by
+        # experiments with this value set to True or only by experiments with this value
+        # set to False.
+        self.bidirectional = False
 
-        max_time = None  # None or a positive float value
-        if max_time is not None:
-            self.extra_opts["gurobi"]["TimeLimit"] = max_time
-            self.extra_opts["knitro"]["maxtime"] = max_time
-            self.extra_opts["bonmin"]["time_limit"] = max_time
-            self.extra_opts["ipopt"]["max_wall_time"] = max_time
+        # Number of multi-starts for the optimization
+        # self.multi_starts = 1
+
+        # Time limits for the solvers
+        # max_time = None
+        # self.extra_opts["gurobi"]["TimeLimit"] = max_time
+        self.extra_opts["knitro"]["maxtime"] = 180
+        # self.extra_opts["bonmin"]["time_limit"] = max_time
+        # self.extra_opts["ipopt"]["max_wall_time"] = max_time
