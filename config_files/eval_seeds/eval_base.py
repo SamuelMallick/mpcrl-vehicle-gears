acb120c58b535ec60f7a6356ba3e2a255af12762
gbattocletti <g.battocletti@tudelft.nl>
2025-06-30 14:11:28 +0200

    Minor update to base eval settings

--- config_files/eval_seeds/eval_base.py
+++ config_files/eval_seeds/eval_base.py
@@ -8,19 +8,15 @@ class EvalBase(ConfigDefault):
         self.id = "eval_base"
         self.eval_seed = 0
 
-        # Toggle for use of bidirectional RNN
-        # NOTE: This value must be kept constant during a set of experiments, i.e., a
-        # batch of experiments that runs at the same time must be either formed only by
-        # experiments with this value set to True or only by experiments with this value
-        # set to False.
+        # Disable bidirectional RNNs
+        # NOTE: this needs to be set to True in case a policy of type c1 or c2 is used
         self.bidirectional = False
 
         # Number of multi-starts for the optimization
-        # self.multi_starts = 1
+        self.multi_starts = 1
 
         # Time limits for the solvers
-        # max_time = None
         # self.extra_opts["gurobi"]["TimeLimit"] = max_time
-        self.extra_opts["knitro"]["maxtime"] = 180
+        self.extra_opts["knitro"]["maxtime"] = 60
         # self.extra_opts["bonmin"]["time_limit"] = max_time
         # self.extra_opts["ipopt"]["max_wall_time"] = max_time
