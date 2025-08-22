2822620efcd341397d3ec922425f0a4b6968d6e9
gbattocletti <g.battocletti@tudelft.nl>
2025-08-22 14:35:01 +0200

    Set time limit for platoon

--- config_files/eval_seeds/eval_time_1s.py
+++ config_files/eval_seeds/eval_time_1s.py
@@ -11,14 +11,14 @@ class Config(Config):
         # Set time limit for the solvers to 1 second
 
         # For single vehicle evaluation (M=1)
-        self.extra_opts["gurobi"]["TimeLimit"] = 1
-        self.extra_opts["knitro"]["maxtime"] = 1
-        self.extra_opts["ipopt"]["max_wall_time"] = 1
+        # self.extra_opts["gurobi"]["TimeLimit"] = 1
+        # self.extra_opts["knitro"]["maxtime"] = 1
+        # self.extra_opts["ipopt"]["max_wall_time"] = 1
 
         # For platooning evaluation (M=5)
-        # self.extra_opts["gurobi"]["TimeLimit"] = 0.2
-        # self.extra_opts["knitro"]["maxtime"] = 0.2
-        # self.extra_opts["ipopt"]["max_wall_time"] = 0.2
+        self.extra_opts["gurobi"]["TimeLimit"] = 0.2
+        self.extra_opts["knitro"]["maxtime"] = 0.2
+        self.extra_opts["ipopt"]["max_wall_time"] = 0.2
 
         # Backup MINLP MPC parameters
         self.backup_minlp_mip_terminate = 1  # stop at 1st feas sol
