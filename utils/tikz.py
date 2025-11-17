4955097f74eeafae8f3aacd772dfb8e08da5fa48
gbattocletti <g.battocletti@tudelft.nl>
2025-11-17 13:59:16 +0100

    Patch tikz export

--- utils/tikz.py
+++ utils/tikz.py
@@ -1,16 +1,18 @@
-import matplotlib as mpl
-
 import tikzplotlib as tikz_lib  # recommended to install tikzplotlib-patched
-
 from matplotlib.figure import Figure
+from matplotlib.legend import Legend
+from matplotlib.lines import Line2D
 
-# monkey patching to fix some issues with tikzplotlib
-# mpl.lines.Line2D._us_dashSeq = property(lambda self: self._dash_pattern[1])
-# mpl.lines.Line2D._us_dashOffset = property(lambda self: self._dash_pattern[0])
-# mpl.legend.Legend._ncol = property(lambda self: self._ncols)
+# Monkey patching to fix some issues with tikzplotlib
+# See https://github.com/nschloe/tikzplotlib/issues/567
+Line2D._us_dashSeq = property(lambda self: self._dash_pattern[1])
+Line2D._us_dashOffset = property(lambda self: self._dash_pattern[0])
+Legend._ncol = property(lambda self: self._ncols)
+
 
 # TODO: consider exporting the data to a separate .dat file instead and importing it
 # in the tikz file to have a more clean code.
+# TODO: consider switching to https://github.com/ErwindeGelder/matplot2tikz
 
 
 def save2tikz(fig: Figure, name: str = "") -> None:
@@ -38,9 +40,23 @@ def save2tikz(fig: Figure, name: str = "") -> None:
     else:
         pass  # name is already a valid tikz file name
 
+    # fix tikzplotlib issues
+    tikzplotlib_fix_ncols(fig)
+
     # save figure
     tikz_lib.save(
         name,
         figure=fig,
         extra_axis_parameters={r"tick scale binop=\times"},
     )
+
+
+def tikzplotlib_fix_ncols(obj):
+    """
+    workaround for matplotlib 3.6 renamed legend's _ncol to _ncols, which breaks
+    tikzplotlib. See: https://stackoverflow.com/questions/75900239/attributeerror-occurs-with-tikzplotlib-when-legend-is-plotted  # pylint: disable=line-too-long
+    """
+    if hasattr(obj, "_ncols"):
+        obj._ncol = obj._ncols  # pylint: disable=protected-access
+    for child in obj.get_children():
+        tikzplotlib_fix_ncols(child)
